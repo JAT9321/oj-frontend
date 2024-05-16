7f3d6ed6be9bafc2ad842c585c6f719d26bd1f61
lab <zgt9321@qq.com>
2024-05-16 22:03:18 +0800

    markdow和代码编辑器使用示例

---
+++ src/views/MarkdownAndCodeEditorExample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="home">
+    <md-editor :value="Mdvalue" :handle-change="MdonChange" />
+    {{ Mdvalue }}
+    <code-editor :value="Codevalue" :handle-change="CodeonChange" />
+    {{ Codevalue }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue";
+
+const Mdvalue = ref();
+const MdonChange = (v: string) => {
+  Mdvalue.value = v;
+};
+
+const Codevalue = ref();
+const CodeonChange = (v: string) => {
+  Codevalue.value = v;
+};
+</script>
